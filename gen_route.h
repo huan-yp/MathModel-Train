0688c52ecb03c82a546ae3ebd966dd594d11f294
huan-yp <3051561876@qq.com>
2025-01-19 14:42:27 +0800

    init

---
+++ gen_route.h
@@ -0,0 +1,8 @@
+#ifndef GEN_ROUTE_H
+#define GEN_ROUTE_H
+
+#include <plain.hpp>
+
+vector<Node> gen_route_access(const Plain &plain, Node start, Node end);
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <gen_route.h>
+
+using namespace std;
+
+int main(){
+    
+
+}
